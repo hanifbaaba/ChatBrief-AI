27deae6d784e051e5929b0b1963756f3c540bd98
hanifubaaba <baabahanif88@gmail.com>
2025-10-28 09:43:09 +0100

    Added JWT authentication and fixed token endpoints

--- backend/chat_brief/urls.py
+++ backend/chat_brief/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path("", include(router.urls)),
     path("register/", UserRegisterView.as_view(), name="register"),
     path("login/",UserLoginView.as_view(), name = "login"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
